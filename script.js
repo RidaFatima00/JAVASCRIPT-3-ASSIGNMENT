8e8e4c8d2e72f4dcf1600188388b4aa213ec75dc
RIDA FATIMA <ridafatimaa.wahab7819@gmail.com>
2026-02-20 12:39:21 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+
+//var age = prompt("Enter your age")
+//if(age>18){
+//console.log( "You are teenager");
+//}
+//else{
+//   console.log("You are not teenager");  
+//}
+
+//var password = prompt("Enter your password")
+//var databasepassword ="123456"
+//if(password === databasepassword){
+//console.log("login seccessful");  
+//}else{
+// console.log("login is not successful");  
+//}
+
+
+//var ticket= prompt("Enter your ticket num")
+//if(ticket !== 435678){
+//console.log("Better luck next time");  
+//}
+
+//var username=prompt("Enter your name")
+//if(username==="Rida"){
+//console.log("Welcome Rida");
+//}
+//else{
+// console.log("Invalid username"); 
+//}
+
+//var num = +prompt("Enter any number")
+// if(num > 0 && num <= 30){
+// console.log("Number is between 1-30");
+ //}else if(num >= 31 && num <= 50){
+// console.log("Number is between 31-50");
+//}else{
+//  console.log("Number is not in between 1-100");
+ //}
+
+
+//var num = +prompt("Enter any number")
+//if(num >= 1 && num <= 20 || num >= 95 && num <= 100){
+//console.log("Number is between 1-20 or 95-100");
+//}else if(num >= 21 && num <= 70){
+// console.log("Number is between 21-70");
+//}else{ 
+//console.log("Number is not in between 1-200");
+//}
